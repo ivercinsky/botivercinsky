fc1b7d0bb7ac92fa84ea8e308c0eea71b9be105e
Ivan Vercinsky <ivercinsky@dc.uba.ar>
2016-09-29 19:55:01 -0300

    primer comit

---
+++ server.js
@@ -0,0 +1,24 @@
+/**
+ * Created by ivan on 29/09/16.
+ */
+'use strict';
+
+var port = process.env.PORT || 8000; // first change
+
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+var server = http.createServer(app);
+
+app.use(bodyParser.json());
+
+
+// change four
+
+
+server.listen(port, function () { // fifth and final change
+});
